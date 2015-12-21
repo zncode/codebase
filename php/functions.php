097f69b52ff5fae3025a4e32167cf961f70e7d27
zncode <2005znwy@163.com>
2015-12-21 17:04:34 +0800

    Update functions.php
    更新

--- php/functions.php
+++ php/functions.php
@@ -51,4 +51,103 @@ function tim2tim($out_time)
 	$string = $day.'天'.$hour.'小时'.$minute.'分钟';
 	return $string;
 }
+
+
+/**
+ * 提交GET请求，curl方法
+ * @param string  $url	   请求url地址
+ * @param mixed   $data	  GET数据,数组或类似id=1&k1=v1
+ * @param array   $header	头信息
+ * @param int	 $timeout   超时时间
+ * @param int	 $port	  端口号
+ * @return array			 请求结果,
+ *							如果出错,返回结果为array('error'=>'','result'=>''),
+ *							未出错，返回结果为array('result'=>''),
+ */
+function curl_get($url, $data = array(), $header = array(), $timeout = 5, $port = 80)
+{
+	$ch = curl_init();
+	if (!empty($data)) {
+		$data = is_array($data)?http_build_query($data): $data;
+		$url .= (strpos($url,'?')?  '&': "?") . $data;
+	}
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_POST, 0);
+	//curl_setopt($ch, CURLOPT_PORT, $port);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION ,1); //是否抓取跳转后的页面
+
+	$result = curl_exec($ch);
+	if (0 != curl_errno($ch)) {
+		$result = "Error:\n" . curl_error($ch);
+
+	}
+	curl_close($ch);
+	return $result;
+}
+
+/**
+ * 提交POST请求，curl方法
+ * @param string  $url	   请求url地址
+ * @param mixed   $data	  POST数据,数组或类似id=1&k1=v1
+ * @param array   $header	头信息
+ * @param int	 $timeout   超时时间
+ * @param int	 $port	  端口号
+ * @return string			请求结果,
+ *							如果出错,返回结果为array('error'=>'','result'=>''),
+ *							未出错，返回结果为array('result'=>''),
+ */
+function curl_post($url, $data = array(), $header = array(), $timeout = 5, $port = 80)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	//curl_setopt($ch, CURLOPT_PORT, $port);
+	!empty ($header) && curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	$result = curl_exec($ch);
+	if (0 != curl_errno($ch)) {
+		$result  = "Error:\n" . curl_error($ch);
+	}
+	curl_close($ch);
+
+	return $result;
+}
+
+/**
+ *  curl put 上传文件
+ * @param <type> $url		  请求url
+ * @param <type> $file		 文件位置
+ * @param <type> $filehandle   文件resource
+ * @param <type> $header	   请求头
+ * @param <type> $timeout	  请求超时限制
+ * @param <type> $port		 请求端口
+ * @return string
+ */
+function curl_put($url, $file,$filehandle, $header = array(), $timeout = 5, $port = 80)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	!empty ($header) && curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_PUT, 1);
+	curl_setopt($ch, CURLOPT_INFILE, $filehandle);
+	curl_setopt($ch, CURLOPT_INFILESIZE, filesize($file));
+
+	$result = curl_exec($ch);
+	if (0 != curl_errno($ch)) {
+		$result = "Error:\n" . curl_error($ch);
+
+	}
+	curl_close($ch);
+	return $result;
+}
+
+
 ?>
