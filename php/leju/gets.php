41756aabf3bc0c892f6a0bec56a660a2560db54f
zncode <2005znwy@163.com>
2016-03-09 10:50:51 +0800

    Update gets.php
    添加读取函数

--- php/leju/gets.php
+++ php/leju/gets.php
@@ -44,3 +44,63 @@ public function get_companys($params = array(), $order='', $page=0, $page_size=1
     return $result;
 }
 
+/**
+* 获取日志列表
+* $type = 1 查询异常表, = 2 查询API表
+*/
+public function get_logs_tag($params = array(), $order='', $page=0, $page_size=10, $type=1, $count=false)
+{
+    switch($type)
+    {
+        case 1:
+        if($count)
+        {
+            $sql = "SELECT COUNT(a.id) as num FROM fnl_log_tags as t LEFT JOIN fnl_abnormal_log as a ON t.data_id = a.id ";
+        }
+        else{
+            $sql = "SELECT t.tag, t.val, t.act, t.type, a.id, a.ctl, a.act, a.msg, a.create_time, a.data, a.code FROM fnl_log_tags as t LEFT JOIN fnl_abnormal_log as a ON t.data_id = a.id ";
+        }
+            break;
+        case 2:
+            if($count)
+            {
+                $sql = "SELECT COUNT(a.id) as num FROM fnl_log_tags as t LEFT JOIN fnl_api_log as a ON t.data_id = a.id ";
+            }
+            else
+            {
+                $sql = "SELECT t.tag, t.val, t.act, t.type, a.id, a.ctl, a.act, a.msg, a.create_time, a.data, a.code FROM fnl_log_tags as t LEFT JOIN fnl_api_log as a ON t.data_id = a.id ";
+            }
+            break;
+    }
+    
+    if(count($params))
+    {
+        $where = ' WHERE ';
+        $where .= implode(' AND ', $params);
+        $sql   .= $where;
+    }
+    
+    if($count)
+    {
+        $result = $this->_module_dboperate->fetchAll($sql);
+        return $result[0]['num'];
+    }
+    
+    if($order)
+    {
+        $sql .= " ORDER BY {$order}";
+    }
+    if($page > 1)
+    {
+        $page = ($page-1)*$page_size;
+    }
+    else
+    {
+        $page = 0;
+    }
+    
+    $sql .= " LIMIT {$page}, {$page_size}";
+    $result = $this->_module_dboperate->fetchAll($sql);
+    return $result;
+}
+
