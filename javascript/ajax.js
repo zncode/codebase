6e222a840f1025f7293cffb784e82c1442c81051
zncode <2005znwy@163.com>
2016-04-01 19:12:27 +0800

    Update ajax.js
    ajax

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,3 +1,11 @@
-tttt
-aaaa
-cccc
+var company_name = $("#company_name").val();
+$.ajax({
+    url: "?site=mobile&ctl=user&act=search_company_name&company_name=" + company_name,
+    dataType: "json",
+    data: {
+        q: request.term
+    },
+    success: function( data ) {
+        response( data );
+    }
+});
