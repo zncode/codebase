83366d67d8eae3e7ad5d4821c43df5e831ab8ba2
zncode <2005znwy@163.com>
2016-06-02 16:47:27 +0800

    Update common.js
    update

--- javascript/native/common.js
+++ javascript/native/common.js
@@ -5,3 +5,11 @@ for (i = 0; i < Obj.length; i++) {
       var login_type = Obj[i].value;
     }
 };
+//ul li 下 a标签添加onclick事件
+var li = document.getElementById("page_video_navigation_ul").getElementsByTagName("a");
+for (var i = 0; i < li.length; i++) {
+    li[i].onclick = function () {
+       var cid = this.attributes["data-category-id"].value;
+       Vercoop.PAGE.Video.videoInnerHtml(cid);
+    }
+}
