48913950f5ebd5518e521c74977b0721d2a8af3b
红蜻蜓 <2005znwy@163.com>
2016-06-14 17:38:15 +0800

    Update common.js
    添加秒转时间函数

--- javascript/native/common.js
+++ javascript/native/common.js
@@ -13,3 +13,24 @@ for (var i = 0; i < li.length; i++) {
        Vercoop.PAGE.Video.videoInnerHtml(cid);
     }
 }
+
+//秒转时间hh:mm:ss
+this.formatTime = function (totalSeconds) {
+    if (totalSeconds < 86400) {
+        var dt = new Date("01/01/2000 0:00");
+
+        dt.setSeconds(totalSeconds);
+        var h = dt.getHours(),
+            m = dt.getMinutes(),
+            s = dt.getSeconds(),
+            r = "";
+        if (h > 0) {
+            r += (h > 9 ? h.toString() : "0" + h.toString()) + ":";
+        }
+        r += (m > 9 ? m.toString() : "0" + m.toString()) + ":"
+        r += (s > 9 ? s.toString() : "0" + s.toString());
+        return r;
+    } else {
+        return null;
+    }
+}
