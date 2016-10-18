a4eca34d2595a1de7c92949dd534184a29e91343
红蜻蜓 <2005znwy@163.com>
2016-10-18 10:17:53 +0800

    Update common.js
    update

--- javascript/common.js
+++ javascript/common.js
@@ -6,3 +6,12 @@ window.open("www.baidu.com");
 
 //解决冲突
 jQuery.noConflict();
+
+//字符串替换（单引号'）
+var columnsStr = columnsStr.replace(/&#039;/g,"\'");
+
+//分割数组(分隔符 冒号:)
+var cArray = columnsArray[i].split(":");
+
+//控制台打印
+console.log();
