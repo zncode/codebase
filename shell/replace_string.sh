f3096146a172ceda700ec0949dd0249bbc7fa5fa
红蜻蜓 <2005znwy@163.com>
2020-08-04 13:44:33 +0800

    Update replace_string.sh

--- shell/replace_string.sh
+++ shell/replace_string.sh
@@ -14,13 +14,13 @@ if [ ! -f "$userini" ];then
   echo "userini文件不存在"
   exit 
 fi
-dayrui=`grep "define('FCPATH', dirname(__FILE__).'/dayrui/')" $index`
+dayrui=`grep "'/../dayrui/'" $index`
 echo $dayrui     
 exit     
 if [ "$dayrui" -eq 0 ];then     27   sed -i "s#define('FCPATH', dirname(__FILE__).'/dayrui/');#define('FCPATH', dirname(__FILE__).'/../dayrui/');\define('MYPATH', WEBPATH.'dayrui/My/');\define('APPSPATH', WEBPATH.'dayrui/App/');        #g" $index     28   echo "成功-index.php内容替换完成！"     29 else
   echo "失败-index.php文件中未找到指定内容！"
 fi
-grep "/www/wwwroot/hb_new/dayrui/" $userini > /dev/null
+grep "/dayrui/" $userini > /dev/null
 if [ $? -eq 0 ];then
   echo "失败-.user.ini已经替换过!"
 else
